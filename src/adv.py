6b1370f2c0bb011fe8950f21e6e7fa762eba01db
Miguel <miguelqnicolas@gmail.com>
2020-04-08 17:52:00 -0400

    player movement functionality completed

--- src/adv.py
+++ src/adv.py
@@ -1,51 +1,76 @@
 from room import Room
+from player import Player
 
-# Declare all the rooms
+# declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
-
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
-
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
-
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
-
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+    'outside': Room("Outside Cave Entrance", "North of you, the cave mount beckons"),
+    'foyer': Room("Foyer", """Dim light filters in from the south. Dusty passages run north and east."""),
+    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm."""),
+    'narrow': Room("Narrow Passage", """The narrow passage bends here from west to north. The smell of gold permeates the air."""),
+    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south."""),
 }
 
-
-# Link rooms together
+# link rooms together
 
 room['outside'].n_to = room['foyer']
+
 room['foyer'].s_to = room['outside']
 room['foyer'].n_to = room['overlook']
 room['foyer'].e_to = room['narrow']
+
 room['overlook'].s_to = room['foyer']
+
 room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
+
 room['treasure'].s_to = room['narrow']
 
-#
-# Main
-#
+# main
 
-# Make a new player object that is currently in the 'outside' room.
+# make a new player object that is currently in the 'outside' room
 
-# Write a loop that:
-#
-# * Prints the current room name
-# * Prints the current description (the textwrap module might be useful here).
-# * Waits for user input and decides what to do.
-#
-# If the user enters a cardinal direction, attempt to move to the room there.
-# Print an error message if the movement isn't allowed.
-#
-# If the user enters "q", quit the game.
+print('\nEnter n, e, s, or w to move your player around.\nEnter q anytime to exit the game.\n')
+name = input('Give your player a name: ')
+player = Player(name, room['outside'])
+print(f'\n{player.name}, are currently in the {player.current_room.name}. {player.current_room.description}.')
+move = input('Where would you like to go? ')
+
+# write a loop that:
+# prints the current room name
+# prints the current description (the textwrap module might be useful here)
+# waits for user input and decides what to do
+
+# if the user enters a cardinal direction, attempt to move to the room there
+# print an error message if the movement isn't allowed
+
+# if the user enters 'q', quit the game
+
+while move != 'q':
+    if move == 'n':
+        try:
+            player.current_room = player.current_room.n_to
+        except:
+            print("\nYou can't go that way, try again.")
+    elif move == 'e':
+        try:
+            player.current_room = player.current_room.e_to
+        except:
+            print("\nYou can't go that way, try again.")
+    elif move == 's':
+        try:
+            player.current_room = player.current_room.s_to
+        except:
+            print("\nYou can't go that way, try again.")
+    elif move == 'w':
+        try:
+            player.current_room = player.current_room.w_to
+        except:
+            print("\nYou can't go that way, try again.")
+    else:
+        print('\nInvalid entry, try again.')
+
+    print(f'\n{player.name}, are currently in the {player.current_room.name}. {player.current_room.description}.')
+    move = input('Where would you like to go? ')
+
+print('\nGame over')
