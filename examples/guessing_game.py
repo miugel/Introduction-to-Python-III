2b570108f8bd0bd7bce4a679010cf8d1e55adfec
Miguel <miguelqnicolas@gmail.com>
2020-04-08 15:38:26 -0400

    file cleanup

--- examples/guessing_game.py
+++ examples/guessing_game.py
@@ -1,28 +1,28 @@
 import random
 
 def guessing_game():
-  print("Guess the number!")
+	print('Guess the number!')
 
-  secret_number = random.randrange(101)
+	secret_number = random.randrange(101)
 
-  while True:
-    guess = input("Input your guess: ")
+	while True:
+    	guess = input('Input your guess: ')
     
     try:
-      guess = int(guess)
+      	guess = int(guess)
     except ValueError:
-      print("Please enter an integer.")
-      continue
+      	print('Please enter an integer.')
+      	continue
 
-    print(f"You guessed: {guess}")
+    print(f'You guessed: {guess}')
 
     if guess == secret_number:
-      print("You win!")
-      break
+      	print('You win!')
+      	break
     elif guess < secret_number:
-      print("Too small!")
+      	print('Too small!')
     else:
-      print("Too big!")
+    	print('Too big!')
 
 if __name__ == '__main__':
-  guessing_game()
+  	guessing_game()
